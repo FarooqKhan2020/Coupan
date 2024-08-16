6731883c7e3abf797a273defcba52dc85eef9230
Techifort <95629259+waqas49m@users.noreply.github.com>
2024-08-16 11:24:45 +0500

    Delete New folder directory
    ext

--- New folder/CategoryPage.jsx
+++
@@ -1,8 +0,0 @@
-import AllCategories from "../Components/Category/AllCategories";
-export default function CategoryPage() {
-  return (
-    <>
-      <AllCategories />
-    </>
-  );
-}
--- New folder/StorePage.jsx
+++
@@ -1,8 +0,0 @@
-import AllStores from "../Components/Store/AllStores";
-export default function CategoryPage() {
-  return (
-    <>
-          <AllStores heading="All Stores" />
-    </>
-  );
-}
